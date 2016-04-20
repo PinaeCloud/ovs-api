19d3b75f1cea46532b92c68bb8b881bdfe46aa36
huiyugeng <interhuiyg@163.com>
2016-04-21 01:00:05 +0800

    Add switch log api

---
+++ ovs/switch.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+
+from ovs import execute
+from ovs.utils import decorator
+
+class SwitchLog():
+    
+    @decorator.check_cmd(['ovs-appctl -V'])
+    def __init__(self):
+        pass
+    
+    @decorator.check_arg
+    def list_log(self):
+        log_lst = {}
+        cmd = 'ovs-appctl vlog/list'
+        result, error = execute.exec_cmd(cmd)
+        if not error:
+            for l in result.split('\n'):
+                items = [item for item in l.strip().split(' ') if item.strip()]
+                if len(items) == 4:
+                    log_lst[items[0]] = {'console':items[1], 'syslog':items[2], 'file':items[3]}
+                    
+        return log_lst
+    
+    @decorator.check_arg
+    def set_log(self, module, destination, level):
+        cmd = 'ovs-appctl vlog/set {0}:{1}:{2}'.format(module, destination, level)
+        _, error = execute.exec_cmd(cmd)
+        return False if error else True
+    
+    @decorator.check_arg
+    def set_log_pattern(self, destination, pattern):
+        cmd = 'ovs-appctl vlog/set PATTERN:{0}:{1}'.format(destination, pattern)
+        _, error = execute.exec_cmd(cmd)
+        return False if error else True
+    
+    @decorator.check_arg
+    def set_log_facility(self, facility):
+        cmd = 'ovs-appctl vlog/set FACILITY:{0}'.format(facility)
+        _, error = execute.exec_cmd(cmd)
+        return False if error else True
+        
---
+++ ovs_test/switch_test.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+
+import unittest
+from ovs import switch
+
+class SwitchLogTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.s = switch.SwitchLog()
+    
+    def test_list_log(self):
+        result = self.s.list_log()
+        self.assertGreater(len(result), 0)
+        module = result.get('bfd')
+        self.assertEquals(module.get('console'), 'OFF')
+        self.assertEquals(module.get('syslog'), 'ERR')
+        self.assertEquals(module.get('file'), 'INFO')
+        
+    def test_set_log(self):
+        if not self.s.set_log('bfd', 'syslog', 'INFO'):
+            self.fail('set_log : set switch log fail')
+            
+        result = self.s.list_log()
+        module = result.get('bfd')
+        self.assertEquals(module.get('syslog'), 'INFO')
+        
+        self.s.set_log('bfd', 'syslog', 'ERR')
+        
