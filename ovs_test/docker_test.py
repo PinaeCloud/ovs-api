dd381272ef836428772fc73a3da31a010fc74a0d
huiyugeng <interhuiyg@163.com>
2016-04-25 18:32:12 +0800

    Update docker_test

--- ovs_test/docker_test.py
+++ ovs_test/docker_test.py
@@ -3,7 +3,6 @@
 import unittest
 from ovs import bridge, docker
 from ovs.utils import execute
-from ovs.utils import ip_utils
 
 class DockerTest(unittest.TestCase):
     
@@ -13,26 +12,27 @@ class DockerTest(unittest.TestCase):
         
     def test_docker_network(self):
         container_name = 'c1'
-        br_name = 'test-br'
-        br_ip = '192.168.150.1/24'
-        c_ip = '192.168.150.3'
-        c_mask = '24'
+        br_name, br_ip = 'test-br', '192.168.150.1/24'
+        c_ip, c_mask = '192.168.150.3', '24'
         
+        c_pid, error = execute.exec_cmd('docker inspect --format={{.State.Pid}} ' + container_name)
+        if not c_pid or error:
+            self.__delete_container(container_name)
+            
         execute.exec_cmd('docker run -d --name {0} --net=none interhui/alpine-ssh /usr/sbin/sshd -D'.format(container_name))
         
-        if not self.b.exists_br(br_name):
-            self.b.add_br(br_name)
-            
-        ip_utils.IFace().startup(br_name)
-        ip_utils.Address().add_addr(br_name, br_ip)
+        self.d.add_network(br_name, br_ip)
         
         self.d.connect(container_name, br_name, '{0}/{1}'.format(c_ip, c_mask))
         result, _ = execute.exec_cmd('ping {0} -c 1 -q | grep packets'.format(c_ip))
         self.assertTrue('0% packet loss' in result.strip())
             
         self.d.disconnect(container_name, br_name)
-        self.b.del_br(br_name)
+        self.d.del_network(br_name)
         
+        self.__delete_container(container_name)
+        
+    def __delete_container(self, container_name):
         execute.exec_cmd('docker stop ' + container_name)
         execute.exec_cmd('docker rm ' + container_name)
         
